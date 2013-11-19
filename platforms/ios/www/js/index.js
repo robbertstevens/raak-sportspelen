666a3779920a740a2235464fefd7f023a07403ae
Michael stevens <m.stevens1990@gmail.com>
2013-11-19 13:50:24 +0100

    Button aan index.htm, test pagina toegevoegd

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -3,5 +3,5 @@ document.addEventListener("DOMContentLoaded", start);
 function start()
 {
     document.getElementById('screen').width = window.innerWidth;
-    document.getElementById('screen').height = window.innerHeight;
+    document.getElementById('screen').height = window.innerHeight;   
 }
